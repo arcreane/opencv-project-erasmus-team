5cb06df70464bd73f1925afb9c32c019064753cf
typkers <katmic4@gmail.com>
2026-05-11 20:49:25 +0200

    feat/fix: complete histogram equalizatio and CLAHE sliders with UI scaling fixes. Fixed UI so the layout is more readable

--- src/EditorModel.cpp
+++ src/EditorModel.cpp
@@ -18,3 +18,34 @@ void EditorModel::applyThreshold(int method, int threshValue) {
    else if (method == 2) cv::adaptiveThreshold(gray, processedImage, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 2);
 }
 
+void EditorModel::applyGlobalEqualization() {
+    if (currentImage.empty()) return;
+
+    cv::Mat gray;
+
+    if (currentImage.channels() == 3) {
+        cv::cvtColor(currentImage, gray, cv::COLOR_BGR2GRAY);
+    }
+    else {
+        gray = currentImage.clone();
+    }
+
+    cv::equalizeHist(gray, processedImage);
+}
+
+void EditorModel::applyCLAHE(double clipLimit, int gridSize) {
+    if (currentImage.empty()) return;
+
+    cv::Mat gray;
+    if (currentImage.channels() == 3) {
+        cv::cvtColor(currentImage, gray, cv::COLOR_BGR2GRAY);
+    }
+    else {
+        gray = currentImage.clone();
+    }
+
+    cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(clipLimit, cv::Size(gridSize, gridSize));
+
+    clahe->apply(gray, processedImage);
+}
+
