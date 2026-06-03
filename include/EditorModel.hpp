c9dfdd61e29fceda4fd09d2a34cdada2c54807f4
toskaz <zaborowskaantosia@gmail.com>
2026-06-03 18:40:14 +0200

    style: format header files

--- include/EditorModel.hpp
+++ include/EditorModel.hpp
@@ -3,24 +3,22 @@
 
 #include <opencv2/opencv.hpp>
 
-
 class EditorModel {
 public:
-    cv::Mat currentImage;
-    cv::Mat processedImage;
+  cv::Mat currentImage;
+  cv::Mat processedImage;
 
-   bool loadImage(const std::string& path);
-   void applyThreshold(int method, int threshValue);
+  bool loadImage(const std::string &path);
+  void applyThreshold(int method, int threshValue);
 
-   void applyGlobalEqualization();
-   void applyCLAHE(double clipLimit, int gridSize);
+  void applyGlobalEqualization();
+  void applyCLAHE(double clipLimit, int gridSize);
 
-   void applyMorphology(int method, int kernelSize, int shapeType);
-   
-   void applyGrabCut(cv::Rect rectangle);
+  void applyMorphology(int method, int kernelSize, int shapeType);
 
-   void applyWhiteBalance(int method);
+  void applyGrabCut(cv::Rect rectangle);
 
+  void applyWhiteBalance(int method);
 };
 
 #endif
--- include/MainWindow.hpp
+++ include/MainWindow.hpp
@@ -12,46 +12,44 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
-    
-private slots:
-    void on_actionOpen_triggered();
-    void on_actionSave_triggered();
-    void on_btnBinary_clicked();
-    void on_btnOtsu_clicked();
-    void on_btnAdaptive_clicked();
-    void updateDisplay();
-    void on_sliderThresh_valueChanged(int value);
-    void on_buttonGlobalEq_clicked();
-    void on_sliderClipLimit_valueChanged(int value);
-    void on_sliderGridSize_valueChanged(int value);
-    void on_btnErosion_clicked();
-    void on_btnDilation_clicked();
-    void on_btnNoise_clicked();
-    void on_btnClosing_clicked();
-    void on_btnGradient_clicked();
-    void on_sliderMorphSize_valueChanged(int value);
-    void on_comboKernelShape_currentIndexChanged(int index);
-    void on_btnApplyWB_clicked();
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow() override;
 
+private slots:
+  void on_actionOpen_triggered();
+  void on_actionSave_triggered();
+  void on_btnBinary_clicked();
+  void on_btnOtsu_clicked();
+  void on_btnAdaptive_clicked();
+  void updateDisplay();
+  void on_sliderThresh_valueChanged(int value);
+  void on_buttonGlobalEq_clicked();
+  void on_sliderClipLimit_valueChanged(int value);
+  void on_sliderGridSize_valueChanged(int value);
+  void on_btnErosion_clicked();
+  void on_btnDilation_clicked();
+  void on_btnNoise_clicked();
+  void on_btnClosing_clicked();
+  void on_btnGradient_clicked();
+  void on_sliderMorphSize_valueChanged(int value);
+  void on_comboKernelShape_currentIndexChanged(int index);
+  void on_btnApplyWB_clicked();
 
 private:
-    Ui::MainWindow* ui;
-    EditorModel* model;
+  Ui::MainWindow *ui;
+  EditorModel *model;
 
-    bool isGrabCutMode = false;
-    bool isDrawingRect = false;
-    QPoint startPoint;
-    QPoint endPoint;
-    cv::Rect grabCutRect;
+  bool isGrabCutMode = false;
+  bool isDrawingRect = false;
+  QPoint startPoint;
+  QPoint endPoint;
+  cv::Rect grabCutRect;
 
-    bool eventFilter(QObject* watched, QEvent* event) override;
-    void updateDisplayWithRect();
+  bool eventFilter(QObject *watched, QEvent *event) override;
+  void updateDisplayWithRect();
 };
 #endif
